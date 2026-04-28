927329624c8ed27c7008bd5cefab1c30d549fc18
NewtonU17 <184322998+NewtonU17@users.noreply.github.com>
2026-04-28 12:28:48 -0500

    Add files via upload

---
+++ test LLM.py
@@ -0,0 +1,52 @@
+from openai import OpenAI
+import pdfplumber  # PyMuPDF
+pdf = pdfplumber.open(r"C:\Users\isaac\OneDrive\Documentos\Futura Demo\Demo\Futura_Talent_Growth_Plan_1year.pdf")
+
+
+full_text = ""
+
+for page in pdf.pages:
+    text = page.extract_text()
+    if text:
+        full_text += text + "\n"
+
+
+    
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
+
+# First API call with reasoning
+response = client.chat.completions.create(
+  model="openai/gpt-oss-120b:free",
+  messages=[
+          {
+            "role": "user",
+            "content": "Summarize the following:" + full_text,
+          }
+        ],
+  
+)
+
+# Extract the assistant message with reasoning_details
+response = response.choices[0].message
+
+# Preserve the assistant message with reasoning_details
+messages = [
+  {"role": "user", "content": "How many r's are in the word 'strawberry'?"},
+  {
+    "role": "assistant",
+    "content": response.content,
+    "reasoning_details": response.reasoning_details  # Pass back unmodified
+  },
+  {"role": "user", "content": "Are you sure? Think carefully."}
+]
+
+response2 = client.chat.completions.create(
+  model="openai/gpt-oss-120b:free",
+  messages=messages,
+  extra_body={"reasoning": {"enabled": True}}
+)
+
+print(response)
